1a5e34c3805dfc4fbe8cf502fda3a439e4e8628f
Robinson Kumar <robinson15kumar@gmail.com>
2024-11-13 21:02:34 +0530

    Update SpringBootEksApplication.java

--- src/main/java/com/javatechie/SpringBootEksApplication.java
+++ src/main/java/com/javatechie/SpringBootEksApplication.java
@@ -12,7 +12,7 @@ public class SpringBootEksApplication {
 
     @GetMapping("/greetings")
     public String message() {
-        return "Welcome to javatechie ! app deployed in AWS EKS";
+        return "Welcome! App deployed in AWS EKS";
     }
 
     public static void main(String[] args) {
